5f3e53ec165ff2b5a143878d1cd1eccc23e106df
Lucas-Graf <53525158+CaptnIglo@users.noreply.github.com>
2023-09-19 18:50:32 +0000

    finished tutorial section

--- myApp.js
+++ myApp.js
@@ -1,39 +1,51 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config();
+let bodyParser = require("body-parser");
 
+app.use("/", bodyParser.urlencoded({ extended: false }));
 
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  },
+);
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", function (req, res) {
+  var msgJson = { message: "Hello json" };
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    msgJson["message"] = msgJson["message"].toUpperCase();
+  }
+  res.json(msgJson);
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/:world/echo", function (req, res) {
+  var world = req.params.world;
+  res.json({ echo: world });
+});
 
+app
+  .route("/name")
+  .get(function (req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    res.json({ name: firstName + " " + lastName });
+  })
+  .post(function (req, res) {
+    var firstName = req.body.first;
+    var lastName = req.body.last;
+    res.json({ name: firstName + " " + lastName });
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
